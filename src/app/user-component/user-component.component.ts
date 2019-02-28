ca2bd374eb0cdbd0e19160c1b6bd751ba8f09b37
mbegolo <marco.begolo@gmail.com>
2019-02-28 09:54:08 +0100

    fixes user-component and editUserForm

--- src/app/user-component/user-component.component.ts
+++ src/app/user-component/user-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../_helpers/must-match.validator';
 import { User } from '../_models';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,8 +16,11 @@ export class UserComponentComponent implements OnInit {
   public closeResult: string;
   public logged_user: User;
   public presentation_name: string;
+  editUserForm: FormGroup;
+  submitted = false;
 
-  constructor(private modalService: NgbModal, private userService: UserService) {}
+  constructor(private formBuilder: FormBuilder, private modalService: NgbModal, private userService: UserService) { }
+
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', centered: true}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
@@ -35,14 +40,30 @@ export class UserComponentComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.initForm();
     this.logged_user = new User();
-    console.log("asdsadsadsadsa: ",this.userService.getLoggedUser());
+    //console.log("asdsadsadsadsa: ",this.userService.getLoggedUser());
     this.logged_user = this.userService.getLoggedUser() as User;
     this.presentation_name = this.logged_user.username;
     if (!(this.logged_user.name === "")) {
       this.presentation_name = this.logged_user.name;
     }
   }
+
+  initForm() {
+    this.editUserForm = this.formBuilder.group({
+        name: [''],
+        surname: [''],
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+    }, {
+        validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  get f() { return this.editUserForm.controls; }
 
   printData() {
     console.log(this.logged_user);
@@ -52,6 +73,7 @@ export class UserComponentComponent implements OnInit {
     console.log("onSave");
     if (!this.save()) this.abort();
     else {
+      this.logged_user.username = this.editUserForm.controls.username.value
       this.userService.updateUser(this.logged_user).subscribe(data => {
         var new_usr = JSON.parse((<any>data)._body);
         console.log(new_usr);
@@ -64,7 +86,9 @@ export class UserComponentComponent implements OnInit {
   }
 
   save() {
-    return confirm("Sicuro di voler salvare? L'azione non è reversibile");
+    //return confirm("Sicuro di voler salvare? L'azione non è reversibile");
+    console.log(this.editUserForm);
+    return false;
   }
 
   onAbort() {
